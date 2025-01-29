c214747ea6c82b42c1471fcaaaad8d7336a7e3b7
Tryptophan36 <shivam@featurely.ai>
2025-01-29 15:08:40 +0530

    stream html temporarily removed

--- src/app/api/streamHtml/route.ts
+++ src/app/api/streamHtml/route.ts
@@ -1,62 +1,62 @@
-import { generateId, createDataStreamResponse, streamText } from 'ai';
-import { Anthropic } from '@anthropic-ai/sdk';
-import { htmlGeneratePrompt } from '@/app/utils/prompts';
-import { systemPromptForHtml } from '@/app/utils/prompts';
-import { anthropic } from "@ai-sdk/anthropic"
+// import { generateId, createDataStreamResponse, streamText } from 'ai';
+// import { Anthropic } from '@anthropic-ai/sdk';
+// import { htmlGeneratePrompt } from '@/app/utils/prompts';
+// import { systemPromptForHtml } from '@/app/utils/prompts';
+// import { anthropic } from "@ai-sdk/anthropic"
 
-const anthropicAI = new Anthropic({
-  apiKey: process.env.ANTHROPIC_API_KEY,
-});
+// const anthropicAI = new Anthropic({
+//   apiKey: process.env.ANTHROPIC_API_KEY,
+// });
 
-export const runtime = 'edge';
+// export const runtime = 'edge';
 
-export async function POST(req: Request) {
-    const { svgCode } = await req.json();
+// export async function POST(req: Request) {
+//     const { svgCode } = await req.json();
     
-    if (!svgCode) {
-      return new Response('Sufficient data not provided', { status: 400 });
-    }
+//     if (!svgCode) {
+//       return new Response('Sufficient data not provided', { status: 400 });
+//     }
 
-    // const result =  streamText({
-    //     model:anthropic("claude-3-5-sonnet-20241022"),
-    //     system:systemPromptForHtml,
-    //     prompt:htmlGeneratePrompt
-    // }
-    // );
+//     // const result =  streamText({
+//     //     model:anthropic("claude-3-5-sonnet-20241022"),
+//     //     system:systemPromptForHtml,
+//     //     prompt:htmlGeneratePrompt
+//     // }
+//     // );
 
-    // return result.toTextStreamResponse();
+//     // return result.toTextStreamResponse();
     
    
-    return createDataStreamResponse({
-        execute: dataStream => {
-          dataStream.writeData('initialized call');
+//     return createDataStreamResponse({
+//         execute: dataStream => {
+//           dataStream.writeData('initialized call');
     
-          const result = streamText({
-            model:anthropic("claude-3-5-sonnet-20241022"),
-            system:systemPromptForHtml,
-           prompt:htmlGeneratePrompt,
-            onChunk() {
-              dataStream.writeMessageAnnotation({ chunk: '123' });
-            },
-            onFinish() {
-              // message annotation:
-              dataStream.writeMessageAnnotation({
-                id: generateId(), // e.g. id from saved DB record
-                other: 'information',
-              });
+//           const result = streamText({
+//             model:anthropic("claude-3-5-sonnet-20241022"),
+//             system:systemPromptForHtml,
+//            prompt:htmlGeneratePrompt,
+//             onChunk() {
+//               dataStream.writeMessageAnnotation({ chunk: '123' });
+//             },
+//             onFinish() {
+//               // message annotation:
+//               dataStream.writeMessageAnnotation({
+//                 id: generateId(), // e.g. id from saved DB record
+//                 other: 'information',
+//               });
     
-              // call annotation:
-              dataStream.writeData('call completed');
-            },
-          });
+//               // call annotation:
+//               dataStream.writeData('call completed');
+//             },
+//           });
     
-          result.mergeIntoDataStream(dataStream);
-        },
-        onError: error => {
+//           result.mergeIntoDataStream(dataStream);
+//         },
+//         onError: error => {
           
-          return error instanceof Error ? error.message : String(error);
-        },
-      });
+//           return error instanceof Error ? error.message : String(error);
+//         },
+//       });
     
     
-  } 
+//   } 
